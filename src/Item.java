258f8297074d5f96beba1ee7c6fba36545fe3a11
Chocow <0nomsi0@gmail.com>
2024-07-17 16:29:52 -0400

    add detailed implementation of all classes and interfaces

--- src/Item.java
+++ src/Item.java
@@ -2,14 +2,17 @@ public class Item {
     private double unitPrice;
     private String name;
 
-    public Item() {
-
+    public Item(double price, String name) {
+        this.unitPrice = price;
+        this.name = name;
     }
 
     public double getUnitPrice() {
+        return this.unitPrice;
     }
 
     public String getName() {
+        return this.name;
     }
 
 }
